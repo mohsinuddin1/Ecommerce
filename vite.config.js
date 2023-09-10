dc7ccd2c2e59754029131ffa407a6ea3a695422b
Md Mohsin <mohsinuddin.handsome@gmail.com>
2023-09-10 16:00:40 +0530

    action gh

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Ecommerce",
-  plugins: [react()],
+
+ 
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Ecommerce/'
+  }
+
+  return config
 })
